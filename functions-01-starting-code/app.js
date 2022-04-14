51d1d82e21ffdd7ac11ae6ff0f7e995d1130ff99
Andrea De Rito <andreaderito95@gmail.com>
2022-04-14 23:00:22 +0200

    prove

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,1 +1,19 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+// const start = function () {
+//   console.log('Game is starting...');
+// };
+
+// const person = {
+//     greet: function greet() {
+//         console.log('Hello there!');
+//     }
+// };
+
+// person.greet();
+
+// console.dir(startGame);
+
+startGameBtn.addEventListener('click', () => {
+    console.log('Game is starting...');
+});
